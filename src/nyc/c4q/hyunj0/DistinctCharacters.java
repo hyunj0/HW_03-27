6356675b14e2693c4965df5bdf87e298836d6661
hyunj0 <hyunj0@hotmail.com>
2015-03-23 20:35:43 -0400

    Worked on Distinct Characters homework with Allison, Alvin, and John

---
+++ src/nyc/c4q/hyunj0/DistinctCharacters.java
@@ -0,0 +1,35 @@
+package nyc.c4q.hyunj0;
+
+import java.util.Scanner;
+
+/**
+ *Created by c4q-hyunj0, alizinha, ahhhlvin, lighterletter
+ **/
+
+//TODOS: work on error message for numbers
+
+public class DistinctCharacters {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a word or sentence");
+        String string = input.next();
+        System.out.println(uniqueCharacters(string));
+
+    }
+
+    public static String uniqueCharacters (String string) {
+        String lowerString = string.toLowerCase();
+        String distinctCharacters = "" + lowerString.charAt(0);
+        for (int i = 1; i < lowerString.length(); i++) {
+            String checkLetter = lowerString.substring(i, i + 1);
+            if (distinctCharacters.contains(checkLetter)) {
+                continue;
+            } else {
+                distinctCharacters = distinctCharacters + lowerString.charAt(i);
+            }
+        }
+        return distinctCharacters;
+
+    }
+}
